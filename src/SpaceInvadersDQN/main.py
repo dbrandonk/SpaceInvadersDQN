892a32eb5dce8b55046023499cc19f753ca24d33
Brandon Knape <d.brandonk@gmail.com>
2023-04-16 17:05:39 -0500

    remove model info from config and runtime errors went away.

--- src/SpaceInvadersDQN/main.py
+++ src/SpaceInvadersDQN/main.py
@@ -8,9 +8,6 @@ config = DEFAULT_CONFIG.copy()
 config['num_workers'] = 1
 config['num_gpus'] = 0
 config['env'] = 'SpaceInvaders-v0'
-config['model']['dim'] = 84
-config['model']['conv_filters'] = [32, 256, 9, 9]
-config['model']['fcnet_hiddens'] = [256]
 config['replay_buffer_size'] = 10000
 config['train_batch_size'] = 32
 config['timesteps_per_iteration'] = 1000
@@ -27,7 +24,7 @@ trainer = DQN(config=config)
 # Train the agent for 100 iterations
 for i in range(100):
     result = trainer.train()
-    print(pretty_print(result))
+    print(result)
 
 # Use the trained agent to play the game
 env = gym.make('SpaceInvaders-v0')
