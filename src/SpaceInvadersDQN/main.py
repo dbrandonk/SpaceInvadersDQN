ed68fcb70557fbd77c2cb6a9d7b8d4d076384190
Brandon Knape <d.brandonk@gmail.com>
2023-05-07 12:12:31 -0500

    WIP. Cleaned up this file to the bare bones.

--- src/SpaceInvadersDQN/main.py
+++ src/SpaceInvadersDQN/main.py
@@ -1,30 +1,23 @@
 import gymnasium as gym
-import torch
 import ray
-from ray.rllib.algorithms.dqn import DQN, DEFAULT_CONFIG
+from ray.rllib.algorithms.dqn import DQN, DQNConfig
+from ray.tune.logger import pretty_print
 
 # Define the configuration for the DQN agent
-config = DEFAULT_CONFIG.copy()
-config['num_workers'] = 1
-config['num_gpus'] = 0
-config['env'] = 'SpaceInvaders-v0'
-config['replay_buffer_size'] = 10000
-config['train_batch_size'] = 32
-config['timesteps_per_iteration'] = 1000
-config['num_steps_sampled_before_learning_starts'] = 1000
-config['exploration_fraction'] = 0.1
-config['exploration_final_eps'] = 0.02
-config['target_network_update_freq'] = 500
-config['framework'] = 'torch'
+config = DQNConfig()
+config.environment('SpaceInvaders-v4')
+config.framework('torch')
 
-# Initialize Ray and create the DQNTrainer object
+# Initialize Ray
 ray.init()
+
+# Create train object.
 trainer = DQN(config=config)
 
 # Train the agent for 100 iterations
 for i in range(100):
     result = trainer.train()
-    print(result)
+    print(pretty_print(result))
 
 # Use the trained agent to play the game
 env = gym.make('SpaceInvaders-v0')
@@ -35,3 +28,4 @@ while not done:
     obs, reward, done, info = env.step(action)
     env.render()
 env.close()
+
