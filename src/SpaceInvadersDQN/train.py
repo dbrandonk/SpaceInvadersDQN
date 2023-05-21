56c93e26902ec21cadb5bf6c9c0ea80700e6b30f
Brandon Knape <d.brandonk@gmail.con>
2023-05-21 14:57:47 -0500

    update toml. exporting trained models. Speced used resources.

--- src/SpaceInvadersDQN/train.py
+++ src/SpaceInvadersDQN/train.py
@@ -5,6 +5,7 @@ config = DQNConfig()
 
 config.environment(env='SpaceInvaders-v4')
 config.framework('torch')
+config.resources(num_learner_workers=8)
 
 space_invader_model = {
     "fcnet_hiddens": [512, 512, 512],
@@ -20,6 +21,8 @@ exploration_config = {
     'epsilon_timesteps': 100000}
 config.exploration(explore=True, exploration_config=exploration_config)
 
+config.checkpointing(export_native_model_files=True)
+
 config.training(
     gamma=0.99,
     lr=0.005,
@@ -28,10 +31,15 @@ config.training(
     optimizer={
         'type': 'Adam'})
 
-trainer = config.build()
+dqn = config.build()
 
 ray.init()
-NUM_EPISODES = 100
+NUM_EPISODES = 200
+CHECKPOINT_RATE = 10
 for episode in range(NUM_EPISODES):
-    result = trainer.train()
+    result = dqn.train()
     print(f"Completed Episode:{episode} of {NUM_EPISODES}")
+
+    if episode % CHECKPOINT_RATE == 0:
+        dqn.save()
+
